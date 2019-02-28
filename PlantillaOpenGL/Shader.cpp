bd7015005eaf89bda812827be4bfc9637abf56fb
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2019-02-28 08:51:23 -0700

    Ya merengues

--- PlantillaOpenGL/Shader.cpp
+++ PlantillaOpenGL/Shader.cpp
@@ -55,10 +55,52 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
 	glCompileShader(fragmentShaderID);
 
 	//4.- Verificar errores de compilacion
+	verificarCompilacion(vertexShaderID);
+	verificarCompilacion(fragmentShaderID);
 
+	//5.- Adjuntar shaders al programa
+	glAttachShader(shaderID, vertexShaderID);
+	glAttachShader(shaderID, fragmentShaderID);
+
+	//6.- Vincular el programa
+	glLinkProgram(shaderID);
+
+	//7.- Verificar la vinculacion
+	verificarVinculacion(shaderID);
+
+	//8.- Usar programa
+	glUseProgram(shaderID);
 }
 
 void Shader::verificarCompilacion(GLuint id) {
 	GLint resultado = GL_FALSE;
 	int longitudLog = 0;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &resultado);
+	glGetShaderiv(id, GL_INFO_LOG_LENGTH, &longitudLog);
+
+	if (longitudLog > 0) {
+		vector<char> mensajeError(longitudLog);
+		glGetShaderInfoLog(id, longitudLog, NULL, &mensajeError[0]);
+		for (vector<char>::const_iterator i = mensajeError.begin();
+			i != mensajeError.end();
+			i++) {
+			cout << *i;
+		}
+	}
+}
+
+void Shader::verificarVinculacion(GLuint id) {
+	GLint estadoVinculacion, estadoValidacion;
+
+	glGetProgramiv(id, GL_LINK_STATUS, &estadoVinculacion);
+	if (estadoVinculacion == GL_FALSE)
+	{
+		cout << "No se pudo vincular programa" << endl;
+	}
+
+	glGetProgramiv(id, GL_VALIDATE_STATUS, &estadoValidacion);
+	if (estadoValidacion == GL_FALSE)
+	{
+		cout << "No se pudo validar la vinculacion" << endl;
+	}
 }
